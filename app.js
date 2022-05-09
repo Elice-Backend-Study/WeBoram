e63665d7b2962e05ee87b0f0ee1d414474ae2eaf
ouivve <ouivve@gmail.com>
2022-05-09 19:42:41 +0900

    http로 서버 띄워보기

--- app.js
+++ app.js
@@ -1,14 +1,28 @@
-const express = require('express');
-const app = express();
-
-app.get('/', (req, res) => {
-  res.send('루트');
+const http = require('http');
+const app = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
+  if (req.url === '/') {
+    res.end('루트');
+  } else if (req.url === '/login') {
+    res.end('로그인');
+  }
 });
 
-app.get('/login', (req, res) => {
-  res.send('로그인');
+app.listen(3001, () => {
+  console.log('http로 서버 가동');
 });
 
-app.listen(3000, () => {
-  console.log('서버 가동');
-});
+// const express = require('express');
+// const app = express();
+
+// app.get('/', (req, res) => {
+//   res.send('루트');
+// });
+
+// app.get('/login', (req, res) => {
+//   res.send('로그인');
+// });
+
+// app.listen(3000, () => {
+//   console.log('서버 가동');
+// });
