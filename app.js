cd6e33ae26537b2f608e720896083d80b5d3dab9
ouivve <ouivve@gmail.com>
2022-05-09 20:03:39 +0900

    라우팅 분리

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
+'use strict';
+// 모듈
 const express = require('express');
 const app = express();
+
+const PORT = 3000;
+// 라우팅
+const home = require('./routes/home');
 
 // 앱 세팅
 app.set('views', './views');
@@ -12,6 +18,8 @@ app.get('/login', (req, res) => {
   res.render('home/login');
 });
 
-app.listen(3000, () => {
+app.use('/', home);
+
+app.listen(PORT, () => {
   console.log('서버 가동');
 });
