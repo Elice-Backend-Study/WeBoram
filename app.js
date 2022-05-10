f39a530537f7e42836da60fe19e6c59a394f1e2c
ouivve <ouivve@gmail.com>
2022-05-10 21:20:13 +0900

    app.listen() 모듈화

--- app.js
+++ app.js
@@ -3,7 +3,6 @@
 const express = require('express');
 const app = express();
 
-const PORT = 3000;
 // 라우팅
 const home = require('./routes/home');
 
@@ -20,6 +19,4 @@ app.get('/login', (req, res) => {
 
 app.use('/', home);
 
-app.listen(PORT, () => {
-  console.log('서버 가동');
-});
+module.exports = app;
